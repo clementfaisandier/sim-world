118a70c642c79ec104001b8b82b5bb9b70a27076
Teariest <clementfaisandier@outlook.com>
2023-12-09 20:15:36 -0800

    Put together the tests for the transformation module, not passing but thats ok. Big thing to remember is that the testee project needs to be setup as .lib so it can be used by the tester project when testing... ideally this would be automated so no headscratching ensues

--- green-sim/src/main.cpp
+++ green-sim/src/main.cpp
@@ -1,17 +1,4 @@
-#include <GL\glew.h>
-#include <GLFW\glfw3.h>
-
-#include <iostream>
-#include <fstream>
-#include "spherical-mesh.h"
-#include "math.h"
-
-#include "transformation-module.h"
-
-
-#include <cmath>
-#define PI 3.14159265
-#define D_TO_RAD 0.01745329251
+#include "main.h"
 
 
 // Error callback functions
---
+++ green-sim/src/main.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+#include <iostream>
+#include <fstream>
+
+#include "spherical-mesh.h"
+#include "transformation-module.h"
+
+#include <cmath>
+
+#define PI 3.14159265
+#define D_TO_RAD 0.01745329251
